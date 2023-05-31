279fa1c6954a6d186e54bb981b47200a74252703
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-05-30 23:02:47 -0300

    Update App.js

--- ex000/my-app/src/App.js
+++ ex000/my-app/src/App.js
@@ -2,11 +2,26 @@
 import './App.css';
 
 function App() {
+
+    const name = "Pedro";
+
+    const newname = name.toUpperCase()
+
+    function sum(a,b){
+      return a + b
+    }
+
+    let url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
       <h1>Olá mundo!</h1>
       <p>meu primeiro App🥹</p>
       <a href="github.com"></a>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newname}</p>
+      <p>soma: {sum(5,5)}</p>
+      <img src={url} alt="minah imagem" ></img>
     </div>
   );
 }
