f87dd3bf6325c25670156c1a197cff63294bc4ce
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-05-30 22:06:52 -0300

    my first steps with React

---
+++ ex000/my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá mundo!</h1>
+      <p>meu primeiro App🥹</p>
+      <a href="github.com"></a>
+    </div>
+  );
+}
+
+export default App;
